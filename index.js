616c1de2a8603725414bac83636a723ad86a7900
mehmood-ahmad <ahmad.khan@keeptruckin.com>
2019-11-20 15:09:32 +0500

    🚧 WIP: initial graphql setup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './data/schema';
 
 const app = express();
 const PORT = 3000;
@@ -6,6 +8,14 @@ const PORT = 3000;
 app.get('/', (req, res) => {
 	res.send(`GraphQL is amazing.`);
 })
+
+const root = { hello: () => "Hello World!" };
+
+app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true,
+}))
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
