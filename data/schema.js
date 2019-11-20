5e9fa1399ff289c7bf891f0b973e621193b4d317
mehmood-ahmad <ahmad.khan@keeptruckin.com>
2019-11-20 15:56:33 +0500

    ✅ DONE: basic graphql schema

--- data/schema.js
+++ data/schema.js
@@ -1,8 +1,17 @@
 import { buildSchema } from 'graphql';
 
 const schema = buildSchema(`
+    type Friend {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: String
+        language: String
+        email: String
+    }
+
     type Query {
-        hello: String
+        friend: Friend
     }
 `)
 
--- index.js
+++ index.js
@@ -9,7 +9,16 @@ app.get('/', (req, res) => {
 	res.send(`GraphQL is amazing.`);
 })
 
-const root = { hello: () => "Hello World!" };
+const root = { friend: () => {
+	return {
+			"id": 28718992,
+			"firstName": "Manny",
+			"lastName": "Henri",
+			"gender": "Male",
+			"language": "English",
+			"email": "me@me.com"
+	}
+} };
 
 app.use('/graphql', graphqlHTTP({
 	schema: schema,
