43747d93d6c0d6e81735df2e819c6a683a582b36
mehmood-ahmad <ahmad.khan@keeptruckin.com>
2019-11-21 22:28:17 +0500

    ➕ ADDS: update and delete mutation

--- data/resolvers.js
+++ data/resolvers.js
@@ -3,10 +3,10 @@ import { Friend } from './dbConnectors';
 // resolver map
 const resolvers = {
 	Query: {
-		getFriend: (root, {id}) => {
+		getFriend: (root, { id }) => {
 			return new Promise((resolve, reject) => {
-				Friend.findById( id, (err, friend) => {
-					if(err) reject(err);
+				Friend.findById(id, (err, friend) => {
+					if (err) reject(err);
 					else resolve(friend);
 				});
 			});
@@ -25,13 +25,29 @@ const resolvers = {
 				contacts: input.contacts,
 			});
 
-			return new Promise( (resolve, reject) => {
+			return new Promise((resolve, reject) => {
 				newFriend.save(err => {
 					if (err) reject(err);
 					else resolve(newFriend);
 				});
 			});
 		},
+		updateFriend: (root, { input }) => {
+			return new Promise((resolve, object) => {
+				Friend.findOneAndUpdate({ _id: input.id }, input, { new: true }, (err, friend) => {
+					if (err) reject(err);
+					else resolve(friend);
+				});
+			});
+		},
+		deleteFriend: (root, { id }) => {
+			return new Promise((resolve, object) => {
+				Friend.remove({ _id: id }, (err) => {
+					if (err) reject(err);
+					else resolve('Successfully deleted');
+				});
+			});
+		}
 	},
 };
 
--- data/schema.js
+++ data/schema.js
@@ -46,6 +46,8 @@ const typeDefs = `
 
     type Mutation {
         createFriend(input: FriendInput): Friend
+        updateFriend(input: FriendInput): Friend
+        deleteFriend(id: ID!): String
     }
 `;
 
